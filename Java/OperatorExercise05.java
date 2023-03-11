d6ed6ab2e8c5b0e72049194277999a715a989948
Ayoung <joahae0302@gmail.com>
2023-03-11 20:18:34 +0900

    연산자 - 십의 자리 이하 버리기

---
+++ Java/OperatorExercise05.java
@@ -0,0 +1,13 @@
+//연산자 
+//십의 자리 이하 버리기
+
+public class OperatorExercise05 {
+
+	public static void main(String[] args) {
+    
+		int value = 356;
+		System.out.println((value / 100) * 100);
+
+	}
+
+}
