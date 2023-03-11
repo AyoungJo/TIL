eaffd21cb50a74926cc1224eaa62bb86846447f3
Ayoung <joahae0302@gmail.com>
2023-03-11 20:19:22 +0900

    연산자 - 사다리꼴 넓이

---
+++ Java/OperatorExercise06.java
@@ -0,0 +1,16 @@
+//연산자
+//사다리꼴 넓이
+
+public class OperatorExercise06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (lengthTop + lengthBottom)*height/2.0;
+		
+		System.out.println(area);
+
+	}
+
+}
