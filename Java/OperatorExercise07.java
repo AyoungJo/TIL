a7ea4bc7974afe60d266b7b104831045eaf8645e
Ayoung <joahae0302@gmail.com>
2023-03-11 20:20:04 +0900

    연산자

---
+++ Java/OperatorExercise07.java
@@ -0,0 +1,13 @@
+//비교 연산자와 논리 연산자 
+
+public class OperatorExercise07 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+
+		System.out.println((x > 7) && (y <= 5)); // true
+		System.out.println((x % 3 == 2) || (y % 2 != 1)); // false
+
+	}
+}
