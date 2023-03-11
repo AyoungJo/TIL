0187638570c065d9adb21d0a89b6d8090e0183aa
Ayoung <joahae0302@gmail.com>
2023-03-11 20:29:48 +0900

    배열 - 최대값 구하기

---
+++ Java/ArrayExercise07.java
@@ -0,0 +1,19 @@
+// 배열, for문
+// 최대값 구하기
+
+public class ArrayExercise07 {
+
+	public static void main(String[] args) {
+		int max = 0;
+		int[] array = { 1, 5, 3, 8, 2 };
+
+		for (int i = 0; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
+			}
+		}
+		System.out.println("max : " + max);
+
+	}
+
+}
