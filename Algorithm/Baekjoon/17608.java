5016e3eb36800256a04decd46e59369f4f4ff9dd
Ayoung <joahae0302@gmail.com>
2023-03-15 21:35:18 +0900

    막대기

---
+++ Algorithm/Baekjoon/17608.java
@@ -0,0 +1,34 @@
+// 17608 - 막대기
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class B17608_막대기_조아영 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<n; i++) {
+			stack.add(sc.nextInt());
+		}
+
+		int cnt = 1;
+
+		int peek = stack.peek();
+
+		for (int i = 0; i < stack.size(); i++) {			
+			if (stack.get(i)> peek) {
+				cnt++;
+			} else {
+				continue;
+			}
+		}
+		System.out.println(cnt);
+
+	}
+
+}
