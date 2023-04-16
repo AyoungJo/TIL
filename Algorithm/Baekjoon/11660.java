9781e34424ee379fb623f8141e531e82a6e7a6d8
Ayoung <joahae0302@gmail.com>
2023-04-16 15:23:10 +0900

    구간 합 구하기 5

--- Algorithm/Baekjoon/11660.java
+++ Algorithm/Baekjoon/11660.java
@@ -1,50 +1,49 @@
-//11660 - 구간 합 구하기5
+//11660 - 구간 합 구하기 5
+
+package april04;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//2차원 구간 합 배열
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 
-		StringTokenizer token = new StringTokenizer(bf.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken()); // 표의 크기
+		int M = Integer.parseInt(st.nextToken()); // 합을 구해야하는 횟수
 
-		int N = Integer.parseInt(token.nextToken());
-		int M = Integer.parseInt(token.nextToken());
+		int[][] A = new int[N + 1][N + 1]; // 숫자 배열 생성
 
-		int[][] arr = new int[N + 1][N + 1];
-
-		for (int i = 0; i < N + 1; i++) {
-			token = new StringTokenizer(bf.readLine(), " ");
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(token.nextToken());
+		for (int i = 1; i < N + 1; i++) {
+			st = new StringTokenizer(bf.readLine(), " ");
+			for (int j = 1; j < N + 1; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		int[][] dp = new int[N + 1][N + 1];
 
-		for (int x = 1; x < N + 1; x++) {
-			for (int y = 1; y < N + 1; y++) {
-				dp[x][y] = dp[x][y - 1] + dp[x - 1][y] - dp[x - 1][y - 1] + arr[x][y];
+		int D[][] = new int[N + 1][N + 1]; // 구간합 배열 생성
+
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < N + 1; j++) {
+				D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
 			}
-
-			System.out.println(Arrays.deepToString(dp));
 		}
 
+		// 총 M줄에 걸쳐 (x1, y1)부터 (x2, y2)까지 합을 구해 출력한다.
 		for (int i = 0; i < M; i++) {
-			token = new StringTokenizer(bf.readLine(), " ");
-			int x1 = Integer.parseInt(token.nextToken());
-			int y1 = Integer.parseInt(token.nextToken());
-			int x2 = Integer.parseInt(token.nextToken());
-			int y2 = Integer.parseInt(token.nextToken());
+			st = new StringTokenizer(bf.readLine(), " ");
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
 
-			int total = dp[x2][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
-
-			System.out.println(total);
+			int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+			System.out.println(result);
 		}
 
 	}
