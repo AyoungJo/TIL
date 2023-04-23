cbe2d3e2f99cc392d46bcd98a1e61cc1d7025b69
Ayoung <joahae0302@gmail.com>
2023-04-23 15:14:43 +0900

    feat : 비밀번호 확인 및 변경

--- Java/JSP/Servlet_Session/src/dao/UserDaoImpl.java
+++ Java/JSP/Servlet_Session/src/dao/UserDaoImpl.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import common.DBManager;
 import dto.UserDto;
@@ -16,6 +17,9 @@ public class UserDaoImpl implements UserDao{
 		return instance;
 	}
 	
+	/**
+	 * 회원가입
+	 */
 	@Override
 	public int register(UserDto userDto) {
 		String sql = "insert into users (user_seq, name, email, phone, password) values (user_seq.nextval, ?, ?, ?, ?) ";
@@ -40,6 +44,68 @@ public class UserDaoImpl implements UserDao{
 		}
 		
 		return ret;
+	}
+	
+	/**
+	 * 비밀번호 일치 여부 확인
+	 */
+	public boolean confirmPassword(int userSeq, String password) {
+		String sql = "select password from users where user_seq = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		boolean result = false;
+		
+		try {
+			con = DBManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, userSeq);
+			
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) {
+				String pw = rs.getString("password");
+				if(password.equals(pw)) {
+					result = true;
+				}				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.releaseConnection(con, pstmt);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 비밀번호 변경
+	 */
+	public int changePassword(int userSeq, String password) {
+		String sql = "update users set password = ? where user_seq = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		int result = 0;
+		
+		try {
+			con = DBManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, password);
+			pstmt.setInt(2, userSeq);		
+
+			result = pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.releaseConnection(con, pstmt);
+		}
+		
+		return result;
 	}
 
 }
