2c8fb76b47ca2cc0f4dd5d95348ba4c41b13a8ea
Ayoung <joahae0302@gmail.com>
2023-04-23 15:13:55 +0900

    feat : 비밀번호 확인 및 변경

--- Java/JSP/Servlet_Session/src/dao/UserDao.java
+++ Java/JSP/Servlet_Session/src/dao/UserDao.java
@@ -4,4 +4,6 @@ import dto.UserDto;
 
 public interface UserDao {
 	int register(UserDto userDto);
+	boolean confirmPassword(int userSeq, String password);
+	int changePassword(int userSeq, String password);
 }
