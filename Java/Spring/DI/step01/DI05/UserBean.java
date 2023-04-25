11b5689ca9a828b3fb55656da1eb39e50614c5be
Ayoung <joahae0302@gmail.com>
2023-04-25 21:00:06 +0900

    UserBean

---
+++ Java/Spring/DI/step01/DI05/UserBean.java
@@ -0,0 +1,34 @@
+package sample05;
+
+public class UserBean {
+  private String id;
+  private String name;
+  private String addr;
+  private String phone;
+  
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {//<property name="id"
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+   
+}
