6a1b45279af1b1ddbb16b1c0cce4c4a60d90da2c
Ayoung <joahae0302@gmail.com>
2023-04-25 21:23:34 +0900

    MessageServiceImpl

---
+++ Java/Spring/AOP/step02Annotation/java/exam/service/MessageServiceImpl.java
@@ -0,0 +1,53 @@
+package exam.service;
+
+import org.springframework.stereotype.Service;
+
+//MessageService service = context.getBean("target", MessageService.class);
+@Service("target")	//id="target" 	
+public class MessageServiceImpl implements MessageService {
+
+	@Override
+	public void korHello() {
+		System.out.println("MessageServiceImpl의 korHello() 메소드 호출");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void engHello(String name) {
+		System.out.println("MessageServiceImpl의 engHello(String name) 메소드 호출");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public int hello() {
+		System.out.println("MessageServiceImpl의 hello() 메소드 호출");		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return 100;
+	}
+
+	@Override
+	public String hello(String name, int age) {
+		System.out.println("MessageServiceImpl의 hello(String name, int age) 메소드 호출");
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "안녕";
+	}
+
+}
