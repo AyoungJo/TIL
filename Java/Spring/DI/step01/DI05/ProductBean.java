49a4bfe4b693830c77f9a9ba5159a45d78950f2b
Ayoung <joahae0302@gmail.com>
2023-04-25 20:59:47 +0900

    ProductBean

---
+++ Java/Spring/DI/step01/DI05/ProductBean.java
@@ -0,0 +1,27 @@
+package sample05;
+
+public class ProductBean {
+	private int pno; //단어_단어 - > setPno();
+	private int price;
+	private String pname;
+
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+  
+}
