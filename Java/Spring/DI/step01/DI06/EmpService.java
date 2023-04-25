f3f9e89fa99cfc208fd20b8f3480df65843a3d99
Ayoung <joahae0302@gmail.com>
2023-04-25 21:02:40 +0900

    EmpService - Annotation 방식

---
+++ Java/Spring/DI/step01/DI06/EmpService.java
@@ -0,0 +1,13 @@
+package sample06;
+
+public class EmpService {
+	
+	public EmpService() {
+		System.out.println("EmpService 생성자 호출");
+	}
+	
+	public void test() {
+		System.out.println("EmpService의 test() 호출입니다");
+	}
+	
+}
