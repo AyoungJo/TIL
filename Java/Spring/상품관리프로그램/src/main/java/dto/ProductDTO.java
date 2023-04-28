38bcf1a19ebe3b181b75f44eb3c4febdf3031696
Ayoung <joahae0302@gmail.com>
2023-04-28 10:45:16 +0900

    fix : @Annotation 추가
    @RequiredArgsConstructor, @NonNull

--- Java/Spring/상품관리프로그램/src/main/java/dto/ProductDTO.java
+++ Java/Spring/상품관리프로그램/src/main/java/dto/ProductDTO.java
@@ -11,14 +11,17 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor //final or @NonNull 필드값만 파라미터로 받는 생성자 생성
 public class ProductDTO {
    private String code;   
-   private String name;   
+   private String name;
+   
+   @NonNull
    private int price;
    private String detail;
    
-	public ProductDTO(String code) {
-		this.code =code;
-	}
+   public ProductDTO(String code) {
+	this.code =code;
+   }
    
 }
