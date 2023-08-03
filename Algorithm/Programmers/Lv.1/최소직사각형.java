ba5b5696da164ca79b353892ac45da38de333365
Ayoung <joahae0302@gmail.com>
2023-08-03 13:12:09 +0900

    최소 직사각형
    풀이 방법 추가

--- Algorithm/Programmers/Lv.1/최소직사각형.java
+++ Algorithm/Programmers/Lv.1/최소직사각형.java
@@ -1,3 +1,4 @@
+//풀이 방법 1.
 import java.util.*;
 
 class Solution {
@@ -19,6 +20,33 @@ class Solution {
             
             maxWidth = Math.max(maxWidth, width);
             maxHeight = Math.max(maxHeight, height);
+        }
+        
+        return maxWidth * maxHeight;
+    }
+}
+
+//풀이 방법 2.
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        
+        int width = 0;
+        int height = 0;
+        
+        //최종 지갑의 가로, 세로 크기
+        int maxWidth = 0;
+        int maxHeight = 0;   
+                
+        for(int i=0; i<sizes.length; i++){            
+            for(int[] size : sizes){
+                width = Math.max(size[0], size[1]);
+                height = Math.min(size[0], size[1]);
+                
+                maxWidth = Math.max(maxWidth, width);
+                maxHeight = Math.max(maxHeight, height);
+            }                        
         }
         
         return maxWidth * maxHeight;
